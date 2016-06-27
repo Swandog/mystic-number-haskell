45e9d44398756c91be9d1e41c70a87a2f332f248
Kenneth Swanson <swandog@gmail.com>
2016-06-27 11:11:10 -0500

    Added method to show a puzzle in a puzzle-ly way

--- mystic.hs
+++ mystic.hs
@@ -1,7 +1,9 @@
 -- file: mystic.hs
 -- lines beginning with "--" are comments.
 
+
 getDir = do
+
   putStrLn "Please enter dir"
   dir <- getLine
   putStrLn ("You Go " ++ dir)
@@ -15,8 +17,10 @@ main = do
   let
     isize = read size :: Int
     puzz = createPuzzle (isize * isize - 1)
-  putStrLn $ show puzz
-  getDir
+--  putStrLn $ show puzz
+  putStr $ showPuzz puzz
+--  getDir puzz
+  getDir puzz
 
 createPuzzle :: Int -> [Int]
 createPuzzle a = constructNextPuzz a [ 0 ]
@@ -24,3 +28,7 @@ createPuzzle a = constructNextPuzz a [ 0 ]
 constructNextPuzz :: Int -> [Int] -> [Int]
 constructNextPuzz 0 xs = xs
 constructNextPuzz a xs = constructNextPuzz (a-1)  (a : xs)
+
+showPuzz :: [Int] -> String
+showPuzz [] = ""
+showPuzz xs = (show $ take 3 xs) ++ "\n" ++ (showPuzz $ drop 3 xs)
